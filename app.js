745f3f5bcb5552c528bb03b3827e26e0089dd03e
xpg3393 <73453814+xpg3393@users.noreply.github.com>
2021-08-10 13:37:10 +1200

    database embedded

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-// app.js
-
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+connectDB();
 
 app.get('/', (req, res) => res.send('Hello world!'));
 
