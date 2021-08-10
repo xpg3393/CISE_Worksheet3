c7a369bb212fc533d5469ea5221e91cd198c8b68
xpg3393 <73453814+xpg3393@users.noreply.github.com>
2021-08-10 14:13:29 +1200

    backend and frontend tied together and working

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const connectDB = require('./config/db');
+var cors = require('cors');
+
+const books = require('./routes/api/books');
 
 const app = express();
 
 connectDB();
 
+// cors
+app.use(cors({ origin: true, credentials: true }));
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('Hello world!'));
+
+// use Routes
+app.use('/api/books', books);
 
 const port = process.env.PORT || 8082;
 
-app.listen(port, () => console.log(`Server running on port ${port}`))
+app.listen(port, () => console.log(`Server running on port ${port}`));
